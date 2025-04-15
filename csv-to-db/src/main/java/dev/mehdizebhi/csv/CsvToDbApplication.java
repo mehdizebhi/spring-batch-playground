d0a14df84d94692cbbb83a305fe4bcf63ab23922
mehdizebhi <mehdizebhi@yahoo.com>
2025-04-15 09:46:54 +0330

    create new spring project

---
+++ csv-to-db/src/main/java/dev/mehdizebhi/csv/CsvToDbApplication.java
@@ -0,0 +1,13 @@
+package dev.mehdizebhi.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvToDbApplication.class, args);
+    }
+
+}
